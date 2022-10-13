d325221468554091f59e31e0469449e51f118fa4
claudia <claudiamagliotti@hotmail.com>
2022-10-13 08:22:55 -0300

    rama actualizada con header en js y comentarios eliminados

---
+++ header.js
@@ -0,0 +1,24 @@
+
+  document.getElementById("encabezado").innerHTML = `<h1 class ="tituloPagina animate__animated animate__backInUp">
+  ECOCIDIO EN EL DELTA
+</h1>
+<h2 style="text-align: right; font-size: 1.5rem; color: black">
+  Concientizando sobre los incendios en los humedales del Delta del Paraná
+  ...
+</h2>
+<nav class="menu">
+  <ol>
+    <li class="menuItem">
+      <a href="Humedales.html">Humedales: Que son?</a>
+    </li>
+    <li class="menuItem"><a href="noticias.html">Noticias</a></li>
+    <li class="menuItem">
+      <a href="galeria.html">Galeria de Imagenes</a>
+    </li>
+    <li class="menuItem"><a href="Registro.html">Contacto</a></li>
+    <li class="menuItem" id="menuInicio">
+            <a href="index.html">Inicio</a>
+          </li>
+  </ol>
+</nav>
+`
