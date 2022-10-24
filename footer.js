06bbb9bff77f01e25c9685811a7818f37b48b165
claudia <claudiamagliotti@hotmail.com>
2022-10-24 05:25:09 -0300

    agregue mensaje en pie de pagina, borre vinculo a imagen vacia de galeria, agregue flecha para imagen firmas

--- footer.js
+++ footer.js
@@ -4,13 +4,30 @@ document.getElementById("pieDePagina").innerHTML = `
         <img
             src="imagenes/twitter1.ico"
             title="twitter"
+            onclick="aunNo()"
          />
           <img
             src="imagenes/facebook1.ico"
             title="facebook"
+            onclick="aunNo()"
           />
           <img
             src="imagenes/instagram.ico"
             title="instagram"
+            onclick="aunNo()"
           />
+          <span id="mensaje" style="visibility:hidden">_   Proximamente ... </span>
+
    `
+  //     function aunNo() {
+  //     window.alert("En breve habilitaremos ...: " );
+  //  }
+  function aunNo() {
+    var mensaje = document.getElementById('mensaje');
+    mensaje.style.visibility = 'visible';
+    function aviso(){
+      mensaje.style.visibility = 'hidden';
+    }
+    
+    setTimeout(aviso, 1500);
+ }
